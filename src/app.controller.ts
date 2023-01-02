a25e71f899eb97e260b809ff471a01e0a051deb6
Moreira <marcelo.moreira@me2.com.br>
2023-01-02 10:42:33 -0300

    iniciando estrutura de REDIS na apĺicação

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, CACHE_MANAGER } from '@nestjs/common';
+import { Cache } from 'cache-manager';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  randomNumDbs = Math.floor(Math.random() * 10);
+  constructor(@Inject(CACHE_MANAGER) private cacheManager: Cache) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('get-number-cache')
+  async getNumber(): Promise<any> {
+    const val = await this.cacheManager.get('number');
+    if (val) {
+      return {
+        data: val,
+        FromRedis: 'this is loaded from redis cache',
+      };
+    }
+
+    if (!val) {
+      await this.cacheManager.set('number', this.randomNumDbs);
+      return {
+        data: this.randomNumDbs,
+        FromRandomNumDbs: 'this is loaded from randomNumDbs',
+      };
+    }
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, CacheModule, CacheInterceptor } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppGateway } from './app/app.gateway';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    CacheModule.register({
+      ttl: 5, // seconds
+      max: 10, // maximum number of items in cache
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService, AppGateway],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CacheInterceptor,
+    },
+    AppService,
+    AppGateway,
+  ],
 })
 export class AppModule {}
